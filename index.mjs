733dca27d3d6b9a150eba2d89956c07db3d9922e
seangjr <sean@relampagos.org>
2023-04-25 18:10:45 +0800

    Modified bot

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import moment from "moment";
 import { Telegraf } from "telegraf";
 import bible from "bible-english";
 
@@ -11,10 +12,6 @@ This bot will cover 1 verse per day of the Psalm 119 challenge, which is to read
 It will start from March 29, 2023 and end on September 20, 2023.
 */
 
-let verse = 28;
-const chapter = 119;
-const book = "psalm";
-
 // formatted date
 const date = new Date().toLocaleDateString("en-US", {
     weekday: "long",
@@ -23,39 +20,62 @@ const date = new Date().toLocaleDateString("en-US", {
     day: "numeric",
 });
 
-bot.start((ctx) => {
+bot.command("help", (ctx) => {
     ctx.replyWithHTML(
-        `This bot will cover 1 verse per day of the Psalm 119 challenge, which is to read 1 verse of Psalm 119 per day for 176 days.\n\nIt will start from <b><u>March 29, 2023</u></b> and end on <b><u>September 20, 2023.</u></b>\n\n<i>Source code: <a>https://github.com/seangjr/psalm-119-bot</a></i>`,
+        `<b>To start, here's the list of the commands:</b>\n/start - Start the bot\n/verse - Get the verse for today`,
     );
-    verseToSend();
 });
 
-// send a message every 24 hours
-setInterval(() => {
-    verseToSend();
-}, 24 * 60 * 60 * 1000);
+bot.command("start", (ctx) => {
+    ctx.replyWithHTML(
+        `This bot will cover 1 verse per day of the Psalm 119 challenge, which is to read 1 verse of Psalm 119 per day for 176 days.\n\nIt will start from <b><u>March 29, 2023</u></b> and end on <b><u>September 20, 2023.</u></b>\n\n<i>Source code: <a>https://github.com/seangjr/psalm-119-bot</a></i>\n\n<b>To start, here's the list of the commands:</b>\n/start - Start the bot\n/verse - Get the verse for today`,
+    );
+    scheduleJob(ctx);
+});
+
+bot.command("/verse", (ctx) => {
+    sendVerseForToday(ctx);
+});
+
+async function sendVerseForToday(ctx) {
+    const now = moment();
+    const tenAM = moment().hour(10).minute(0).second(0);
+    const daysSinceStart = now.diff(moment("2023-03-29"), "days");
+    const daysUntil = moment("2023-09-20").diff(now, "days");
+    const verseNumber = daysSinceStart + 1;
+
+    if (now.isSameOrAfter(tenAM)) {
+        await bible.getVerse(`Psalm 119:${verseNumber}`, (err, data) => {
+            if (err) throw err;
+            const c = data[0];
+            bot.telegram.sendMessage(
+                ctx.chat.id,
+                `Today is <b>${date}</b>\n\n<b>Psalm 119:${verseNumber}</b>\n<i>${c.text}</i>`,
+                { parse_mode: "HTML" },
+            );
+        });
+    }
+
+    if (daysUntil === 0) {
+        bot.telegram.sendMessage(
+            ctx.chat.id,
+            `<b>Today is the last day of the challenge!</b>\n\n<i>Source code: <a>https://github.com/seangjr/psalm-119-bot</a></i>`,
+            { parse_mode: "HTML" },
+        );
+    }
+}
+
+function scheduleJob(ctx) {
+    const now = moment();
+    const tenAM = moment().hour(10).minute(0).second(0);
+    const delay = tenAM.diff(now, "milliseconds");
+
+    setTimeout(() => {
+        sendVerseForToday(ctx);
+        setInterval(() => {
+            sendVerseForToday(ctx);
+        }, 24 * 60 * 60 * 1000);
+    }, delay);
+}
 
 bot.launch(console.log("Bot is running"));
-
-function verseToSend() {
-    if (verse > 176) {
-        verse = 1;
-    }
-    bible.getVerse(`${book} ${chapter}:${verse}`, (err, data) => {
-        if (err) throw err;
-        const c = data[0];
-        bot.telegram.getUpdates().then((updates) => {
-            updates.forEach((update) => {
-                const chat = update.message.chat;
-                if (chat === "group" || chat === "supergroup") {
-                    bot.telegram.sendMessage(
-                        chat.id,
-                        `Today is ${date}. Let's read together!\n\n<b>${c.bookname} ${c.chapter}:${c.verse}</b>\n${c.text}\n\n<i>Do share what you've taken away from reading this word too!</i>`,
-                        { parse_mode: "HTML" },
-                    );
-                }
-            });
-        });
-    });
-    verse++;
-}
